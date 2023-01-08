e0fea90a57ab96d3ab787eeef0bf325aad3f19df
unknown <bassel_sabour@hotmail.com>
2023-01-08 13:11:52 +0200

    code improvements

--- lib/screens/bottom_navigation_bar_persistent_screen/my_bottom_navigation_bar.dart
+++ lib/screens/bottom_navigation_bar_persistent_screen/my_bottom_navigation_bar.dart
@@ -9,43 +9,43 @@ class BottomNavigationBarPersistentScreen extends StatelessWidget {
   final PersistentTabController _controller =
       PersistentTabController(initialIndex: 1);
 
-  List<Widget> _buildScreens() {
-    return [InfoDisplayScreen(), InfoDisplayScreen(), SettingsScreen()];
-  }
+  final List<Widget> _buildScreens = [
+    InfoDisplayScreen(),
+    InfoDisplayScreen(),
+    SettingsScreen()
+  ];
 
-  List<PersistentBottomNavBarItem> _navBarsItems() {
-    return [
-      PersistentBottomNavBarItem(
-        icon: const Icon(Icons.favorite),
+  final List<PersistentBottomNavBarItem> _navBarsItems = [
+    PersistentBottomNavBarItem(
+      icon: const Icon(Icons.favorite),
+      activeColorPrimary: kActiveLabelColor,
+      inactiveColorPrimary: kInactiveLabelColor,
+    ),
+    PersistentBottomNavBarItem(
+        icon: const Icon(
+          Icons.home,
+          color: kActiveLabelColor,
+        ),
+        inactiveIcon: const Icon(
+          Icons.home,
+          color: kInactiveLabelColor,
+        ),
         activeColorPrimary: kActiveLabelColor,
         inactiveColorPrimary: kInactiveLabelColor,
-      ),
-      PersistentBottomNavBarItem(
-          icon: const Icon(
-            Icons.home,
-            color: kActiveLabelColor,
-          ),
-          inactiveIcon: const Icon(
-            Icons.home,
-            color: kInactiveLabelColor,
-          ),
-          activeColorPrimary: kActiveLabelColor,
-          inactiveColorPrimary: kInactiveLabelColor,
-          activeColorSecondary: Colors.amber),
-      PersistentBottomNavBarItem(
-        icon: const Icon(Icons.settings),
-        activeColorPrimary: kActiveLabelColor,
-        inactiveColorPrimary: kInactiveLabelColor,
-      ),
-    ];
-  }
+        activeColorSecondary: Colors.amber),
+    PersistentBottomNavBarItem(
+      icon: const Icon(Icons.settings),
+      activeColorPrimary: kActiveLabelColor,
+      inactiveColorPrimary: kInactiveLabelColor,
+    ),
+  ];
 
   PersistentTabView MyPersistentTabView(context) {
     return PersistentTabView(
       context,
       controller: _controller,
-      screens: _buildScreens(),
-      items: _navBarsItems(),
+      screens: _buildScreens,
+      items: _navBarsItems,
       backgroundColor: kBottomNavBarColor,
       popActionScreens: PopActionScreensType.all,
       itemAnimationProperties: const ItemAnimationProperties(
